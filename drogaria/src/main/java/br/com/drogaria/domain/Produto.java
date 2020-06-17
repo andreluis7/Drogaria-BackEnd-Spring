676849b0f2f73d98e2be76a2cbd25b68352e21a2
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-16 21:26:35 -0300

    (#7) Adicionando a dependencia lombock para o projeto

--- drogaria/src/main/java/br/com/drogaria/domain/Produto.java
+++ drogaria/src/main/java/br/com/drogaria/domain/Produto.java
@@ -3,6 +3,13 @@ package br.com.drogaria.domain;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Produto {
 	
 	private Short codigo;
