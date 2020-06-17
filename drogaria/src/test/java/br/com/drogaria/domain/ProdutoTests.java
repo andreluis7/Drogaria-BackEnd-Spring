a81469f72973d7fdf4c4344179b7f550ded5b7f7
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-16 21:53:42 -0300

    (#8) Teste da entidade produto

---
+++ drogaria/src/test/java/br/com/drogaria/domain/ProdutoTests.java
@@ -0,0 +1,23 @@
+package br.com.drogaria.domain;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProdutoTests {
+
+	@Test
+	public void criar() {
+		Produto p = new Produto( Short.valueOf("1"),
+							      "Coca-Cola", 
+								  Byte.valueOf("15"), 
+								  BigDecimal.valueOf(10.50),
+								  LocalDate.of(2020, 6, 16));
+		
+		System.out.println(p);
+	}
+	
+}
