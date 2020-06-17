f2ba16beb4e63e91773b04c293bde4f3248134ab
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-16 22:24:54 -0300

    (#12) Testando produtorepository

---
+++ drogaria/src/test/java/br/com/drogaria/repository/ProdutoRepositoryTests.java
@@ -0,0 +1,34 @@
+package br.com.drogaria.repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.drogaria.domain.Produto;
+
+@SpringBootTest
+public class ProdutoRepositoryTests {
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
+	@Test
+	public void inserir() {
+		Produto p1 = new Produto( null,
+			      "Coca-Cola", 
+				  Byte.valueOf("15"), 
+				  BigDecimal.valueOf(10.50),
+				  LocalDate.of(2020, 6, 16));
+		
+		Produto p2 = new Produto( null,
+			      "Fanta", 
+				  Byte.valueOf("10"), 
+				  BigDecimal.valueOf(12.70),
+				  LocalDate.of(2020, 6, 17));
+		
+		produtoRepository.save(p1);
+		produtoRepository.save(p2);
+	}
+}
