3e3ce86e762fc20eb7793fcdca91420dd2c0d3d1
andreluis7 <48221325+andreluis7@users.noreply.github.com>
2020-06-20 12:39:34 -0300

    (#19) Remover e editar categoria

--- drogaria/src/main/java/br/com/drogaria/controller/CategoriaController.java
+++ drogaria/src/main/java/br/com/drogaria/controller/CategoriaController.java
@@ -1,10 +1,14 @@
 package br.com.drogaria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,4 +35,15 @@ public class CategoriaController {
 		return categoriaSalva;
 	}
 	
+	@DeleteMapping("/{codigo}")
+	public Categoria remover (@PathVariable Short codigo) {
+		Optional<Categoria> categoriaEncontrada = categoriaRepository.findById(codigo);
+		categoriaRepository.delete(categoriaEncontrada.get());
+		return categoriaEncontrada.get();
+	}
+	
+	@PutMapping
+	public Categoria editar(@RequestBody Categoria categoria) {
+		return categoriaRepository.save(categoria);
+	}
 }
